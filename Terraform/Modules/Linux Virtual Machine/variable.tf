fd08451df73eea0835a8f14771aeb8ede8590de3
djh82uk <djh82uk@gmail.com>
2021-05-12 14:21:49 +0100

    Create variable.tf

---
+++ Terraform/Modules/Linux Virtual Machine/variable.tf
@@ -0,0 +1,14 @@
+variable "vm_nic_name" {}
+variable "resourcegroup" {}
+variable "location" {default = "west europe"}
+variable "ip_name" {}
+variable "addr_allocation" {default = "dynamic"}
+variable "vm_name" {}
+variable "vm_size" {}
+variable "vm_admin_user" {}
+variable "osdisk_caching" {}
+variable "storage_account_type" {}
+variable "image_publisher" {}
+variable "image_offer" {}
+variable "image_sku" {}
+variable "image_version" {}
