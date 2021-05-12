f48f8d40fa801bd8f31b1ab2f7e627ad08a5bb1d
djh82uk <djh82uk@gmail.com>
2021-05-12 13:46:11 +0100

    Update deploy.tf

--- Terraform/0001- VM Deployment 01/deploy.tf
+++ Terraform/0001- VM Deployment 01/deploy.tf
@@ -20,7 +20,7 @@ resource "azurerm_resource_group" "RG1" {
 
 module "VNet"{
 source = "../Modules/Virtual Network/"
-vnet_name       = "GitHub-Test-VNet"
+vnet_name       = "${azurerm_virtual_network.VNET1.name}"
 resourcegroup   = "${azurerm_resource_group.RG1.name}"
 addrspace       = ["10.0.0.0/16"]
 location        = "${azurerm_resource_group.RG1.location}"
