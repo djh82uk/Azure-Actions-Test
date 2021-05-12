6e82276f3c74de5be1ea782c09252666a0fdd9cc
djh82uk <djh82uk@gmail.com>
2021-05-12 12:30:49 +0100

    Update VNet.tf

--- Terraform/Modules/Virtual Network/VNet.tf
+++ Terraform/Modules/Virtual Network/VNet.tf
@@ -1,13 +1,13 @@
 resource "azurerm_virtual_network" "VNET1" {
-  name                = "GitHub-Test-VNet"
-  address_space       = ["10.0.0.0/16"]
-  location            = "west europe"
-  resource_group_name = "GitHub-Test-RG"
+  name                = "${var.vnet_name}"
+  address_space       = "${var.addrspace}"
+  location            = "${var.location}"
+  resource_group_name = "${var.resourcegroup}"
 }
 
 resource "azurerm_subnet" "VNET1" {
-  name                 = "VMs"
-  resource_group_name  = "GitHub-Test-RG"
-  virtual_network_name = "GitHub-Test-VNet"
-  address_prefixes     = ["10.0.2.0/24"]
+  name                 = "${var.subnet_name}"
+  resource_group_name  = "${var.resourcegroup}"
+  virtual_network_name = "${var.vnet_name}"
+  address_prefixes     = "${var.addrprefixes}"
 }
