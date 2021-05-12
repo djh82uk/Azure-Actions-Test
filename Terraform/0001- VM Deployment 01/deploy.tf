02e07a6001d6db2e7301da950b952f9394b46c01
djh82uk <djh82uk@gmail.com>
2021-05-12 12:47:39 +0100

    Update deploy.tf

--- Terraform/0001- VM Deployment 01/deploy.tf
+++ Terraform/0001- VM Deployment 01/deploy.tf
@@ -1,7 +1,5 @@
 terraform {
   backend "azurerm" {
-    source  = "hashicorp/azurerm"
-    version = "=2.46.0"
     resource_group_name  = #{resource_group_name}#
     storage_account_name = #{storage_account_name}#
     container_name       = #{container_name}#
