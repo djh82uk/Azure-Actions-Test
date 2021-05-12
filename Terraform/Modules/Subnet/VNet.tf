7c3484338b3e679b0d5db7111de85080e7d7bd89
djh82uk <djh82uk@gmail.com>
2021-05-12 15:01:07 +0100

    Changed Subnet Module

---
+++ Terraform/Modules/Subnet/VNet.tf
@@ -0,0 +1,13 @@
+resource "azurerm_virtual_network" "VNET1" {
+  name                = "${var.vnet_name}"
+  address_space       = "${var.addrspace}"
+  location            = "${var.location}"
+  resource_group_name = "${var.resourcegroup}"
+}
+
+resource "azurerm_subnet" "VNET1" {
+  name                 = "${var.subnet_name}"
+  resource_group_name  = "${var.resourcegroup}"
+  virtual_network_name = "${azurerm_virtual_network.VNET1.name}"
+  address_prefixes     = "${var.addrprefixes}"
+}
